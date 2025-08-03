b5a7e898484ba515f5a3f956c20a5cf589a9682d
Andrii Yushchyk <andriiyuschyk@gmail.com>
2025-08-04 01:04:02 +0300

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('cypress');
+require('dotenv').config();
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: process.env.BASE_URL,
+        setupNodeEvents(on, config) {
+        },
+        specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'cypress/support/e2e.js',
+        screenshotsFolder: 'cypress/screenshots',
+        videosFolder: 'cypress/videos',
+        screenshotOnRunFailure: true,
+        video: true,
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    watchForFileChanges: true,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+});
+
